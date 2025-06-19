e39d9b76555b362f9fbf664202f13d612711d2ca
rb58853 <rb58853@gmail.com>
2025-06-19 09:13:14 -0400

    Refactorized

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from src.supabase_connect import test as test_supabase_connect
-from src.db_connect import test as test_db_connect
+from src.test_connection.supabase_connect import test as test_supabase_connect
+from src.test_connection.db_connect import test as test_db_connect
 
 
 test_supabase_connect()
