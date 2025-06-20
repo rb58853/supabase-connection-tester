91becf9b781ad5d69436a07102753a96ead53eca
rb58853 <rb58853@gmail.com>
2025-06-20 17:02:03 -0400

    Add httpx_client_connection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.test_connection.supabase_connect import test as test_supabase_connect
 from src.test_connection.db_connect import test as test_db_connect
-
+from src.test_connection.httpx_client import test as test_httpx_client
 
 test_supabase_connect()
 test_db_connect()
+test_httpx_client()
--- src/test_connection/httpx_client.py
+++ src/test_connection/httpx_client.py
@@ -1,14 +1,16 @@
 import httpx
 import os
+import asyncio
 
-supabase_access_token: str = os.getenv("SUPABASE_ACCESS_TOKEN")
-supabase_api_url: str = "https://api.supabase.io"
+supabase_access_token: str = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+supabase_api_url: str = "https://suparaul.differential.es"
 
 
 def create_httpx_client(supabase_access_token, supabase_api_url) -> httpx.AsyncClient:
     """Create and configure an httpx client for API requests."""
     headers = {
         "Authorization": f"Bearer {supabase_access_token}",
+        "apikey": supabase_access_token,
         "Content-Type": "application/json",
     }
 
@@ -20,4 +22,18 @@ def create_httpx_client(supabase_access_token, supabase_api_url) -> httpx.AsyncC
 
 
 def test():
-    create_httpx_client(supabase_access_token, supabase_api_url)
+    async def test_users_table():
+        async with create_httpx_client(
+            supabase_access_token, supabase_api_url
+        ) as client:
+            # Cambia 'users' por el nombre real de una de tus tablas
+            response = await client.get("/rest/v1/users")
+            print(
+                "✓ HTTPX connection established successfully"
+                if response.status_code == 200
+                else "✗ HTTPX connection established error"
+            )
+            # print("Status code:", response.status_code)
+            # print("Response JSON:", response.json())
+
+    asyncio.run(test_users_table())
